2d8559ddc86076cad6f669a38cf3013f4a601512
MugishaU <muwiragiye123@gmail.com>
2020-08-06 13:10:06 +0100

    change title of document and initial render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 class App extends React.Component {
+  state = {};
   render() {
     return (
       <main>
         <h1>Mugisha's React App</h1>
-        <h2>Off you go</h2>
+        <h2>Happy Coding</h2>
       </main>
     );
   }
