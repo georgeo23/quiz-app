cdf354ba89b293253e5a035704a63e87b9394324
MugishaU <muwiragiye123@gmail.com>
2020-08-06 18:11:14 +0100

    add test suite including test coverage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ class App extends React.Component {
   state = {};
   render() {
     return (
-      <main>
+      <>
         <h1>Mugisha's React App</h1>
         <h2>Happy Coding</h2>
-      </main>
+      </>
     );
   }
 }
