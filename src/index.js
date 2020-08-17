fb85288f5777abf9c448bb9414a5713b40475aee
Anna Tran <annatran@annas-air.mynet>
2020-08-17 17:02:43 +0100

    remove import store.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import { createStore, applyMiddleware } from 'redux'
-// import { Provider } from 'react-redux'
-// import thunk from 'redux-thunk'
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import Reducer from './Reducer/Reducer';
-
-
-// const store = createStore(Reducer, composeWithDevTools(applyMiddleware(thunk)));
+import { Provider } from 'react-redux'
+import store from './store';
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            {/* <Provider store={store}> */}
+            <Provider store={store}>
                 <App />    
-            {/* </Provider> */}
+            </Provider>
         </BrowserRouter>
     </React.StrictMode>,
  document.getElementById("root"));
