121735030f6c21966477e90b76e75397e3e8c284
Anna Tran <annatran@annas-air.mynet>
2020-08-17 17:06:55 +0100

    add instructions and back button

---
+++ src/Pages/HowToPlay.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import BackButton from '../components/BackButton.js'
+const HowToPlay = ({ history }) => {
+    return (
+        <div>
+            <BackButton />
+                <h2>How to play:</h2>
+                    <p>Select up to 5 players to quiz with as well as how many questions you want to answer each.</p>
+                    <p>Choose your favourite topic for the quiz, or go rogue and choose one you are less familiar with!</p>
+                    <p>Finally - choose your difficulty! Whether you're new to quizzes or you all fancy a challenge on your favourite topics you can choose how easy or hard you want it!</p>
+        </div>
+    )
+}
+
+export default withRouter(HowToPlay);
