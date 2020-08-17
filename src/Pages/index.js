bbd7582f2ba47e5f0654af6e9b46595ae68ec72e
Anna Tran <annatran@annas-air.mynet>
2020-08-17 17:05:12 +0100

    create index.js

---
+++ src/Pages/index.js
@@ -0,0 +1,5 @@
+export { default as HowToPlay } from './HowToPlay';
+export { default as Play } from './Play';
+export { default as Questions } from './Questions';
+export { default as Result } from './Result';
+export { default as Welcome } from './Welcome';
