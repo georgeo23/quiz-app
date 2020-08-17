470b8d55fe5d52069cdbd109ee4ac9690af9ae67
Anna Tran <annatran@annas-air.mynet>
2020-08-17 17:03:18 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
+//Components
+import { HowToPlay, Welcome, Play, Questions, Result } from './Pages';
+
 
 class App extends React.Component {
 
   render() {
     return (
       <>
-        <h1>Quiz</h1>
+        <Switch>
+          <Route exact path='/' component={ Welcome } />
+          <Route path='/howto' component={ HowToPlay } />
+          <Route path='/play' component={ Play } /> 
+          <Route path='/questions' component={ Questions } />
+          <Route path='/result' component={ Result } />
+        </Switch>
+       
       </>
     );
   }
