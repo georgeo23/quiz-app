2ef1519de77dd25e6537070a50527e02337fcf60
Anna Tran <annatran@annas-air.mynet>
2020-08-18 16:08:00 +0100

    comment out strict mode

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { Provider } from 'react-redux'
 import store from './store';
 
 ReactDOM.render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
                 <App />    
             </Provider>
-        </BrowserRouter>
-    </React.StrictMode>,
+        </BrowserRouter>,
+    // </React.StrictMode>,
  document.getElementById("root"));
