be105b1a3b95e87e8d0a4c55b545108d03a1529b
Anna Tran <annatran@annas-air.mynet>
2020-08-18 10:21:57 +0100

    update questions page

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,5 @@
 export { default as HowToPlay } from './HowToPlay';
 export { default as Play } from './Play';
-export { default as Questions } from './Questions';
+export { default as QuestionPage } from './QuestionPage';
 export { default as Result } from './Result';
 export { default as Welcome } from './Welcome';
