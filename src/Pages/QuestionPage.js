6a61fb0542f3545f257687a56924d8a5477ceb4e
Anna Tran <annatran@annas-air.mynet>
2020-08-18 16:07:34 +0100

    remove next button and tidy code

--- src/Pages/QuestionPage.js
+++ src/Pages/QuestionPage.js
@@ -1,82 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { getQuizData } from '../Actions/Actions'
-import Questions from '../Components/Questions.js'
+import Question from '../Components/Question.js'
 
 class QuestionPage extends React.Component {
 
-    renderResults = results => {
+    state = this.props.state;
 
-     
-        console.log(results)
-        console.log(this.props.data)
-        // let keys = Object.keys(resultData)
-        // console.log(keys)
-        
-        let result = JSON.stringify(results)
-        console.log(result)
-        let convert = result.replace(/&#039/g, "").replace(/&quot/g, "").replace(/&/g, "").replace(/;/g, '').replace(/Ex;/g, '').replace(/&rsquos/g, '').replace(/&ldquo/g, '').replace(/&shy;/g, '')
+    renderResults = () => {
 
-        let newResult = JSON.parse(convert);
-        console.log(newResult.length)
-        // for (let i = 0; i < newResult.length; i++) {
-        //     // newResult[i]
-        //     console.log(newResult[0].question)
-        //     // return ( <div></div> )
-        // }
-        // let question = newResult.filter((k => k.startsWith('question')))
-        // console.log(question)
-
-        // let resultData = newResult.map((item, index, resp) => (
-        //     <div key={index}>
-        //         <span>{item.question}</span>
-        //         <h2 style={{ visibility: 'hidden'}}>{resp = [item.correct_answer, item.incorrect_answers[0], item.incorrect_answers[1], item.incorrect_answers[2]]}</h2>
-        //         <h2>{resp[Math.floor(Math.random() * 4)]}</h2>
-        //         <button onClick={ this.nextQuestion }>Next Question</button> 
-        //     </div>
-        // ))
-     
-        let questions = newResult.map((item, idx) => <div key={idx}><p>{item.question}</p></div>)
-        let answers = newResult.map((item,idx) => <div key={idx}><p>{item.correct_answer}</p></div>)
-        // let incorrectAnswers = item.incorrect_answers;
-
-        // let incorrectAnswers = () => {for (let i=0; i < item.incorrect_answers.length; i++ ) {
-        //     newResult.map((item,idx) => <div key={idx}><p>{item.incorrect_answers[i]}</p></div>)
-        // }}
-
-        let incorrectAnswers = newResult.map((item,idx) => {for (let i=0; i < item.incorrect_answers.length; i++) {return item.incorrect_answers[i]}})
-
-        // console.log(typeof incorrectAnswers[0])
-        let keys = Object.values(incorrectAnswers)
-        console.log(typeof keys)
-        // console.log()
-
-        return (
-            <div>
-                <h1>{questions[0]}</h1>
-                <ul>
-                <li>{answers[0]}</li>
-                <li>{keys[0]}</li>
-                <li>{keys[1]}</li>
-                <li>{keys[2]}</li>
-                </ul>
-
-
-            </div>
-            
-        )
+        // console.log(this.props.data[i])
+        if(this.props.data.length){
+            for (let i= 0; i < this.props.data.length; i++) {
+            console.log(this.props.data[i])
+            return <Question question={this.props.data[i].question} correctAnswer={this.props.data[i].correct_answer} incorrectAnswers={this.props.data[i].incorrect_answers} currentQuestion={this.props.currentQuestion} players={this.props.players} score={this.props.score} totalScore={this.props.addScore}/>
+            }
+        }
+   
     }
 
         
     render() {
+        const renderQs = this.props.data.map((q, idx) => <Question key={idx} question={q} correctAnswer={q} incorrectAnswers={q} player={q}/>)
+
+       
         return (
             <div>
-                <Questions player={this.props.players} currentQuestion={this.props.currentQuestion}
-                question={this.props.data.question}
-                correctAnswer={this.props.data.correct_answer}/>
-                <button onClick={this.props.nextQuestion}>Next</button>
+                <div> {this.renderQs}</div>
+                {this.props.data.length > 0 ? this.renderResults() : <p>Please load questions from play game</p>}
                 
-                {this.props.data ? this.renderResults(this.props.data) : <p>load questions</p>}
+                
             </div>
         )
     }
@@ -90,8 +42,7 @@ const mSTP = state => ({
 
 })
 
-const mDTP = dispatch => ({
-    // selectAnswer: answer => dispatch({type: '', payload: answer}),
+export const mDTP = dispatch => ({
     addScore: () => dispatch({type: 'ADD_SCORE', payload: 1}),
     nextQuestion: () => dispatch({type: 'NEXT_QUESTION'})    
 })
