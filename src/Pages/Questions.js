014affa22f85f0e3e1acd45ec350c85b9c3f1d45
Anna Tran <annatran@annas-air.mynet>
2020-08-18 10:22:47 +0100

    rename file

--- src/Pages/Questions.js
+++ src/Pages/Questions.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 // import { getQuizData } from '../Actions/Actions'
+import Questions from '../Components/Questions.js'
 
-class Questions extends React.Component {
+class QuestionPage extends React.Component {
 
     renderResults = results => {
-        // const entities = {
-        //     '&#039;': "'",
-        //     '&quot;': '"',
-        //     // add more if needed
-        //   };
 
-
+     
+        console.log(results[0])
+        // let keys = Object.keys(resultData)
+        // console.log(keys)
+        
         let result = JSON.stringify(results)
-        let convert = result.replace(/&#039/g, "").replace(/&quot/g, "").replace(/;/g, '').replace(/Ex;/g, '').replace(/&rsquos/g, '').replace(/&ldquo/g, '').replace(/&shy;/g, '')
-        // let convert = result.replace(/[#&;\\039\\ldquo]/g, '')
-        let convert2 = result.replace(/&\\quot/g, "")
-
-        // "Where is the train station &quot;Llanfair&shy;pwllgwyngyll&shy;gogery&shy;chwyrn&shy;drobwll&shy;llan&shy;tysilio&shy;gogo&shy;goch&quot;?"
+        let convert = result.replace(/&#039/g, "").replace(/&quot/g, "").replace(/&/g, "").replace(/;/g, '').replace(/Ex;/g, '').replace(/&rsquos/g, '').replace(/&ldquo/g, '').replace(/&shy;/g, '')
 
         let newResult = JSON.parse(convert);
-        
+        console.log(newResult.length)
+        // for (let i = 0; i < newResult.length; i++) {
+        //     // newResult[i]
+        //     console.log(newResult[0].question)
+        //     // return ( <div></div> )
+        // }
+        // let question = newResult.filter((k => k.startsWith('question')))
+        // console.log(question)
 
-        // newRes = () => (
-        //     <h1>{this.newResult}</h1>
-        // )
-        // return <h1>{this.newResult}</h1>
-        return newResult.map((item, index, resp) => (
+        let resultData = newResult.map((item, index, resp) => (
             <div key={index}>
-               
                 <span>{item.question}</span>
-                <h2 >{resp = [item.correct_answer, item.incorrect_answers[0], item.incorrect_answers[1], item.incorrect_answers[2]]}</h2>
-{/*                 style={{ visibility: 'hidden'}}
+                <h2 style={{ visibility: 'hidden'}}>{resp = [item.correct_answer, item.incorrect_answers[0], item.incorrect_answers[1], item.incorrect_answers[2]]}</h2>
+                <h2>{resp[Math.floor(Math.random() * 4)]}</h2>
+                <button onClick={ this.nextQuestion }>Next Question</button>
+{/*              
                 <h2>{resp[Math.floor(Math.random() * 4)]}</h2>
                 <h2>{resp[Math.floor(Math.random() * 4)]}</h2>
                 <h2>{resp[Math.floor(Math.random() * 4)]}</h2>
@@ -46,39 +46,27 @@ class Questions extends React.Component {
                 
             </div>
         ))
+     
+
+        return (
+            <div></div>
+        )
     }
 
         
     render() {
         return (
             <div>
-                <h1>Players: {this.props.players}</h1>
-                {/* <h1>Question: {this.props.data}</h1> */}
-                {/* {console.log(this.props.data.question)} */}
-                <h1>Question: {this.props.currentQuestion+1}</h1>
+                <Questions 
+                on_submit={this.nextQuestion}
+                question_number={this.props.currentQuestion + 1}
+                player={this.props.player}
+                />
                 <button onClick={this.props.nextQuestion}>Next</button>
-                {/* <span onClick={this.props.addScore}></span> */}
-                {/* <p>Score: {this.props.score}</p> */}
+                
                 {this.props.data ? this.renderResults(this.props.data) : <p>load questions</p>}
             </div>
         )
     }
 }
     
- const mSTP = state => ({
-    data: state.data,
-    players: state.players,
-    score: state.score,
-    currentQuestion: state.currentQuestion
-
-})
-
-const mDTP = dispatch => ({
-    // selectAnswer: answer => dispatch({type: '', payload: answer}),
-    addScore: () => dispatch({type: 'ADD_SCORE', payload: 1}),
-    nextQuestion: () => dispatch({type: 'NEXT_QUESTION'})    
-})
-
-export default connect(mSTP, mDTP) (Questions);
-
-
