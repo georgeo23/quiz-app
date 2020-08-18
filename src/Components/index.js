115d3272e8be128a9247ab528d42972945e0faea
Anna Tran <annatran@annas-air.mynet>
2020-08-18 10:18:46 +0100

    export question component

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export { default as Category } from './Category';
 export { default as Difficulty } from './Difficulty';
 export { default as Players } from './Players';
+export { default as Questions } from './QuestionPage';
