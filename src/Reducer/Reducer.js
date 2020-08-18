0c5c12aee5016079f17a29123d27350243aa8a03
Anna Tran <annatran@annas-air.mynet>
2020-08-18 18:25:27 +0100

    update

--- src/Reducer/Reducer.js
+++ src/Reducer/Reducer.js
@@ -1,4 +1,4 @@
-const initialState = { data: [], players: 0, score: 0, currentQuestion: 0 };
+const initialState = { data: [], players: 0, score: 0, currentQuestionID: 0 };
 
 const Reducer = (state = initialState, action) => {
     switch(action.type) {
@@ -9,7 +9,7 @@ const Reducer = (state = initialState, action) => {
         case 'ADD_SCORE':
             return { ...state, score: action.payload + state.score}
         case 'NEXT_QUESTION':
-            return { ...state, currentQuestion: state.currentQuestion+= 1 }
+            return { ...state, currentQuestionID: state.currentQuestionID+= 1 }
         case 'RESET_GAME':
             return { ...state }
         default:
