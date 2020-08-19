487bd13dcab39c82b2a97279781a623869cd6c3a
Anna Tran <annatran@annas-air.mynet>
2020-08-19 17:57:05 +0100

    show correct answer

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -8,22 +8,35 @@ class Question extends Component {
     constructor() {
         super();
         this.state = {
-            currentPlayer: 'Player 1'
+            currentPlayer: 'Player 1',
+            buttonPressed: false
         };
 
     this.handleNextQuestion = this.handleNextQuestion.bind(this);
     this.scoreChecker = this.scoreChecker.bind(this);
 }
-    componentDidMount = () => console.log(this.props)
+    componentDidMount = () => {document.getElementById('nextq').style = "visibility: hidden"; document.getElementById('correctAnswerh1').style = "visibility: hidden"}
      
-    showNextButton = (p) => {
-        document.getElementById('nextq').style = "visibility: visible"
-        return p
+    showNextButton = () => {
+        
+            // document.getElementById(`answerBtn${i}`).setAttribute= ("visibility: hidden")
+            document.getElementById('nextq').style = "visibility: visible"
+            document.getElementById('correctAnswerh1').style = "visibility: visible"
+        
     }
 
     scoreChecker = (selectedAnswer, player) => {
-        console.log(selectedAnswer)
-        console.log(player)
+        // document.querySelectorAll('answerBtn').style = "visibility: hidden"
+        for (let i = 1; i < 5; i++) {
+            console.log(document.getElementById(`answerBtn${i}`))
+            document.getElementById(`answerBtn${i}`).style= "visibility: hidden"
+            console.log(`The correct answer was: ${this.props.question.correct_answer}`)
+            // if (this.state.buttonPressed) {
+            //     this.setState({buttonPressed: true})
+            // }
+        }
+       
+        
         
             if (player === `Player 1`) {
                 if (selectedAnswer === this.props.question.correct_answer) {
@@ -43,14 +56,19 @@ class Question extends Component {
             } else if (player === `Player 4`) {
                 if (selectedAnswer === this.props.question.correct_answer) {
                     this.props.addPlayerScore4()
+                    
                     console.log(`Player 4's score is ${this.props.players[3].score}`)
                 } 
             } else if (player === `Player 5`) {
                 if (selectedAnswer === this.props.question.correct_answer) {
                     this.props.addPlayerScore5()
+                    
                     console.log(`Player 5's score is ${this.props.players[4].score}`)
                 } 
             }
+            // else if (this.state.buttonPressed) {
+            //     return this.setState({buttonPressed: true})
+            // } 
             else {
                 console.log('finished')
             }
@@ -82,10 +100,13 @@ class Question extends Component {
                 }
         
        this.props.nextQuestion()
-       
+       for (let i = 1; i < 5; i++) {
+        console.log(document.getElementById(`answerBtn${i}`))
+        document.getElementById(`answerBtn${i}`).style= "visibility: visible"
         document.getElementById('nextq').style.visibility = "hidden"
+        document.getElementById('correctAnswerh1').style = "visibility: hidden"
     }
-      
+}
     render() {
         const shuffledAnswer = [this.props.question.correct_answer, ...this.props.question.incorrect_answers].sort(() => Math.random() - 0.5);
         
@@ -94,13 +115,17 @@ class Question extends Component {
                     <h1>Q{this.props.currentQuestionID+1}/{this.props.data.length}</h1>
                     <h1>{this.state.currentPlayer} of {this.props.noOfPlayers}</h1>
                     <h1 dangerouslySetInnerHTML={{ __html: this.props.question.question }}></h1>
-
-                <button onClick={() => this.scoreChecker(shuffledAnswer[0], this.state.currentPlayer)} >{shuffledAnswer[0]}</button> 
-                <button onClick={() => this.scoreChecker(shuffledAnswer[1],  this.state.currentPlayer)} >{shuffledAnswer[1]}</button> 
-                <button onClick={() => this.scoreChecker(shuffledAnswer[2],  this.state.currentPlayer)} >{shuffledAnswer[2]}</button> 
-                <button onClick={() => this.scoreChecker(shuffledAnswer[3],  this.state.currentPlayer)} >{shuffledAnswer[3]}</button> 
-                <button onClick={() => this.handleNextQuestion(this.state.currentPlayer)} id="nextq" style={{visibility: "hidden"}} >NEXT QUESTION</button>
+            
+                <button id="answerBtn1" dangerouslySetInnerHTML={{ __html: shuffledAnswer[0] }} onClick={() => this.scoreChecker(shuffledAnswer[0], this.state.currentPlayer)} ></button> 
+                <button id="answerBtn2" dangerouslySetInnerHTML={{ __html: shuffledAnswer[1] }} onClick={() => this.scoreChecker(shuffledAnswer[1],  this.state.currentPlayer)} ></button> 
+                <button id="answerBtn3" dangerouslySetInnerHTML={{ __html: shuffledAnswer[2] }} onClick={() => this.scoreChecker(shuffledAnswer[2],  this.state.currentPlayer)} ></button> 
+                <button id="answerBtn4" dangerouslySetInnerHTML={{ __html: shuffledAnswer[3] }} onClick={() => this.scoreChecker(shuffledAnswer[3],  this.state.currentPlayer)} ></button> 
+                <button id="nextq" onClick={() => this.handleNextQuestion(this.state.currentPlayer)}>NEXT QUESTION</button>
+                <h1 dangerouslySetInnerHTML={{ __html: `The correct answer was ${this.props.question.correct_answer}` }}id="correctAnswerh1"></h1>
             </div>
+           
+               
+            
         )
     }
 }
