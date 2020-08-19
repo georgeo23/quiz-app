62b6a336e90ee69baef4426e48aeb4feb8305bec
Anna Tran <annatran@annas-air.mynet>
2020-08-19 15:36:39 +0100

    create function for reset game on reducer

--- src/Actions/Actions.js
+++ src/Actions/Actions.js
@@ -1,19 +1,4 @@
 export const loading = (p) => ({type: "LOAD_PLAYER", payload: p})
-//    return async dispatch => {
-        
-//    }
-
-
-
-// export const loadPlayerScore = (p) => {
-//     // console.log(p)
-//     for (let i = 1; i <= p; i++)  ({
-//         type: 'ADD_PLAYERS_SCORES',
-//         payload: `player${i}_score`
-    
-        
-//     })
-
 
 export const loadQuiz = (data) => ({
     type: 'LOAD_QUIZ',
@@ -24,14 +9,11 @@ export const getQuizData = (origData) => {
     
     return async dispatch => {
         dispatch(loading(origData.players))
-        // dispatch(loadPlayerScore(origData.players))
     try {
         const quiz = await fetch(`https://opentdb.com/api.php?amount=${origData.questions}&category=${origData.category}&difficulty=${origData.difficulty}&type=multiple`)
-        
         const data = await quiz.json()
         console.log(data.results)
-        dispatch(loadQuiz(data.results))
-        
+        dispatch(loadQuiz(data.results))     
     } catch (err) {
         console.warn(err.message)
     }    
@@ -43,4 +25,4 @@ export const addPlayerScore3 = () => ({type: "ADD_PSCORE3" })
 export const addPlayerScore4 = () => ({type: "ADD_PSCORE4" })
 export const addPlayerScore5 = () => ({type: "ADD_PSCORE5" })
 export const nextQuestion = () => ({type: "NEXT_QUESTION"})
-// export const updateScore = () => ({type: 'UPDATE_PLAYERS_SCORE'})
+export const resetGame = () => ({type: "RESET_GAME"})
