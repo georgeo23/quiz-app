3a9b8e916697ede9b96e9259d9cc6ee6d39f91aa
Anna Tran <annatran@annas-air.mynet>
2020-08-19 13:57:35 +0100

    create functions from the reducer

--- src/Actions/Actions.js
+++ src/Actions/Actions.js
@@ -1,4 +1,19 @@
 export const loading = (p) => ({type: "LOAD_PLAYER", payload: p})
+//    return async dispatch => {
+        
+//    }
+
+
+
+// export const loadPlayerScore = (p) => {
+//     // console.log(p)
+//     for (let i = 1; i <= p; i++)  ({
+//         type: 'ADD_PLAYERS_SCORES',
+//         payload: `player${i}_score`
+    
+        
+//     })
+
 
 export const loadQuiz = (data) => ({
     type: 'LOAD_QUIZ',
@@ -9,7 +24,7 @@ export const getQuizData = (origData) => {
     
     return async dispatch => {
         dispatch(loading(origData.players))
-   
+        // dispatch(loadPlayerScore(origData.players))
     try {
         const quiz = await fetch(`https://opentdb.com/api.php?amount=${origData.questions}&category=${origData.category}&difficulty=${origData.difficulty}&type=multiple`)
         
@@ -22,5 +37,10 @@ export const getQuizData = (origData) => {
     }    
 }
 }
-
+export const addPlayerScore1 = () => ({type: "ADD_PSCORE1" })
+export const addPlayerScore2 = () => ({type: "ADD_PSCORE2" })
+export const addPlayerScore3 = () => ({type: "ADD_PSCORE3" })
+export const addPlayerScore4 = () => ({type: "ADD_PSCORE4" })
+export const addPlayerScore5 = () => ({type: "ADD_PSCORE5" })
 export const nextQuestion = () => ({type: "NEXT_QUESTION"})
+// export const updateScore = () => ({type: 'UPDATE_PLAYERS_SCORE'})
