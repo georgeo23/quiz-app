b2f18913857090ebec404e25bde422f228e4d751
Anna Tran <annatran@annas-air.mynet>
2020-08-20 16:19:27 +0100

    tidy code

--- src/Pages/HowToPlay.js
+++ src/Pages/HowToPlay.js
@@ -23,10 +23,6 @@ const HowToPlay = ({ history }) => {
                             </li>
                         </ol>
                 </div>
-
-                    {/* <p>Select up to 5 players to quiz with as well as how many questions you want to answer each.</p>
-                    <p>Choose your favourite topic for the quiz, or go rogue and choose one you are less familiar with!</p>
-                    <p>Finally - choose your difficulty! Whether you're new to quizzes or you all fancy a challenge on your favourite topics you can choose how easy or hard you want it!</p> */}
         </div>
     )
 }
